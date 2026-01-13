63d99cbe31a50822c5b78877dcc774439e2ff2df
Rahi Sadat Ruhan <rahisadat7@gmail.com>
2026-01-13 11:59:40 +0600

    Added navigation to Patient Dashboard

--- app/src/main/java/com/example/ruhan_2207088_hospital_management_system/PatientDashboardActivity.java
+++ app/src/main/java/com/example/ruhan_2207088_hospital_management_system/PatientDashboardActivity.java
@@ -1,31 +1,56 @@
 package com.example.ruhan_2207088_hospital_management_system;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+
 public class PatientDashboardActivity extends AppCompatActivity {
 
-    private TextView lblPatientName;
-    private String patientId;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_dashboard);
 
-        lblPatientName = findViewById(R.id.lblPatientName);
+        Toolbar toolbar = findViewById(R.id.patient_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.patient_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.patient_nav_view);
 
 
-        String name = getIntent().getStringExtra("p_name");
-        patientId = getIntent().getStringExtra("p_id");
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        if (name != null) {
-            lblPatientName.setText("Welcome, " + name);
-        }
 
-        findViewById(R.id.btnLogout).setOnClickListener(v -> {
-            finish();
+        View headerView = navigationView.getHeaderView(0);
+        TextView lblPatientName = headerView.findViewById(R.id.lblPatientName);
+        String name = getIntent().getStringExtra("patientName");
+        if (name != null) lblPatientName.setText("Welcome, " + name);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_patient_profile) {
+
+            } else if (id == R.id.nav_book_appointment) {
+
+            } else if (id == R.id.nav_patient_logout) {
+                finish();
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
         });
     }
 }
